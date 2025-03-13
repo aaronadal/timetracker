25582da93525d1df83e6672dece7d809390bd1b4
Aarón Nadal <aaronadal.dev@gmail.com>
2025-03-13 20:17:54 +0100

    feat: create list all controller

---
+++ backend/app/User/ListAll.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\User;
+
+use Core\Auth\Application\Query\AllUsersQuery;
+use Core\Shared\Domain\Bus\QueryBusInterface;
+use Core\Shared\Infrastructure\Symfony\Controller\ApiResponse;
+use Core\Shared\Infrastructure\Symfony\Controller\ViewApiResponse;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route('/auth/users', methods: ['GET'])]
+final class ListAll
+{
+    public function __invoke(
+        QueryBusInterface $queryBus,
+    ): ApiResponse {
+        $users = $queryBus->ask(new AllUsersQuery());
+
+        return new ViewApiResponse($users);
+    }
+}
