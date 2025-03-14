1563e61a77546a05ed24c41d393d562c60003570
Aarón Nadal <aaronadal.dev@gmail.com>
2025-03-14 20:49:39 +0100

    fix: make work entry end field nullable

--- backend/migrations/Version20250314090926.php
+++ backend/migrations/Version20250314090926.php
@@ -17,16 +17,17 @@ final class Version20250314090926 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('CREATE TABLE tracking__work_entries (
-          id CHAR(36) NOT NULL,
-          user CHAR(36) NOT NULL,
-          start INT NOT NULL,
-        end INT NOT NULL,
-        created_at INT NOT NULL,
-        updated_at INT NOT NULL,
-        deleted_at INT DEFAULT NULL,
-        INDEX idx_user (user),
+            id CHAR(36) NOT NULL,
+            user CHAR(36) NOT NULL,
+            start INT NOT NULL,
+            end INT DEFAULT NULL,
+            created_at INT NOT NULL,
+            updated_at INT NOT NULL,
+            deleted_at INT DEFAULT NULL,
+            INDEX idx_user (user),
         PRIMARY KEY(id))DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
     }
+
 
     public function down(Schema $schema): void
     {
