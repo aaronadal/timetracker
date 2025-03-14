bd1ff364e48234b4bfb2d779aac10b2538ec35e7
Aarón Nadal <aaronadal.dev@gmail.com>
2025-03-14 18:10:11 +0100

    feat: add delete work entry command

---
+++ backend/src/Tracking/Application/Command/DeleteWorkEntryCommand.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Core\Tracking\Application\Command;
+
+use Core\Shared\Domain\Bus\CommandInterface;
+use Core\Tracking\Domain\Entity\WorkEntryId;
+use Core\Tracking\Domain\Entity\WorkEntryUserId;
+
+final class DeleteWorkEntryCommand implements CommandInterface
+{
+    public function __construct(
+        public readonly WorkEntryId $id,
+        public readonly WorkEntryUserId $user,
+    )
+    {
+    }
+}
---
+++ backend/src/Tracking/Application/Command/DeleteWorkEntryHandler.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Core\Tracking\Application\Command;
+
+use Core\Shared\Domain\Bus\CommandHandlerInterface;
+use Core\Shared\Domain\Bus\QueryBusInterface;
+use Core\Shared\Domain\Exception\EntityNotFound;
+use Core\Tracking\Application\Service\UserExistsGuard;
+use Core\Tracking\Domain\Entity\WorkEntry;
+use Core\Tracking\Domain\Entity\WorkEntryId;
+use Core\Tracking\Domain\Entity\WorkEntryUserId;
+use Core\Tracking\Domain\Persistence\WorkEntryRepositoryInterface;
+
+final class DeleteWorkEntryHandler implements CommandHandlerInterface
+{
+    private readonly UserExistsGuard $userExistsGuard;
+
+    public function __construct(
+        private readonly WorkEntryRepositoryInterface $repo,
+        QueryBusInterface $queryBus,
+    )
+    {
+        $this->userExistsGuard = new UserExistsGuard($queryBus);
+    }
+
+    public function __invoke(DeleteWorkEntryCommand $command): void
+    {
+        $this->guardUserExists($command->user);
+        $entry = $this->guardWorkEntryExists($command->id, $command->user);
+
+        $entry->deleted();
+
+        $this->repo->save($entry);
+
+        // TODO: Publish Domain Events.
+    }
+
+    private function guardUserExists(WorkEntryUserId $user): void
+    {
+        ($this->userExistsGuard)($user);
+    }
+
+    private function guardWorkEntryExists(WorkEntryId $id, WorkEntryUserId $user): WorkEntry
+    {
+        $result = $this->repo->matching(['id' => $id->value(), 'user' => $user->value(), 'deletedAt' => null]);
+        if(count($result) === 0) {
+            throw EntityNotFound::forClassAndId(WorkEntry::class, $id);
+        }
+
+        return $result[0];
+    }
+}
