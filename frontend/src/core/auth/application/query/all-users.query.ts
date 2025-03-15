72e73bbbfc96a3fe997b77f2ab6ab1c2172f1203
Aarón Nadal <aaronadal.dev@gmail.com>
2025-03-15 20:06:18 +0100

    feat(frontend): add all users query

---
+++ frontend/src/core/auth/application/query/all-users.query.ts
@@ -0,0 +1,12 @@
+import type {UserRepositoryInterface} from "@/core/auth/domain/repository/user.repository.ts";
+import type {User} from "@/core/auth/domain/entity/user.ts";
+
+export class AllUsersQuery {
+  public constructor(
+    private readonly repo: UserRepositoryInterface,
+  ) {}
+
+  async run(): Promise<User[]> {
+    return this.repo.all();
+  }
+}
