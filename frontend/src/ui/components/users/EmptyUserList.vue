6da6b80ddf4fcc5c10f420764282e3d981a746c8
Aarón Nadal <aaronadal.dev@gmail.com>
2025-03-15 20:22:21 +0100

    feat(frontend): add empty user list component

---
+++ frontend/src/ui/components/users/EmptyUserList.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import UserIcon from "@/ui/components/shared/icons/UserIcon.vue";
+import Button from "@/ui/components/shared/Button.vue";
+import Card from "@/ui/components/shared/Card.vue";
+
+const emit = defineEmits<{
+  (evt: 'create'): void,
+}>()
+</script>
+
+<template>
+  <Card class="flex justify-center h-full items-center">
+    <div class="content-center flex flex-col items-center gap-4">
+      <p class="text-3xl font-bold text-primary">Aún no tienes usuarios creados</p>
+      <Button type="primary" @click="emit('create')">
+        <UserIcon />
+        Crear nuevo
+      </Button>
+    </div>
+  </Card>
+</template>
