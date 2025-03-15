5e6f87670796b8374fb98fcfc96fd90c44ac7316
Aarón Nadal <aaronadal.dev@gmail.com>
2025-03-15 20:06:11 +0100

    feat(frontend): add sign up command

---
+++ frontend/src/core/auth/application/command/sign-up.command.ts
@@ -0,0 +1,12 @@
+import type {UserRepositoryInterface} from "@/core/auth/domain/repository/user.repository.ts";
+import type {User} from "@/core/auth/domain/entity/user.ts";
+
+export class SignUpCommand {
+  public constructor(
+    private readonly repo: UserRepositoryInterface,
+  ) {}
+
+  async run(name: string): Promise<User> {
+    return this.repo.create(name);
+  }
+}
