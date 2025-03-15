cadd4b1934f7f7c42e206bc988976a637180ab81
Aarón Nadal <aaronadal.dev@gmail.com>
2025-03-15 20:23:11 +0100

    feat(frontend): add no users view

--- frontend/src/ui/App.vue
+++ frontend/src/ui/App.vue
@@ -1,8 +1,20 @@
 <script setup lang="ts">
+import {container} from "@/core/container.ts";
+import type { User } from "@/core/auth/domain/entity/user";
+import { ref } from "vue";
+import NoUsersView from "@/ui/views/NoUsersView.vue";
+
+const query = container.get('Auth.User.Query.All')
+const users = ref<User[]>([]);
+const ready = ref(false);
+
+query.run()
+  .then((response) => users.value = response)
+  .then(() => ready.value = true);
 </script>
 
 <template>
-  <div>
-    Hello world!
-  </div>
+  <template v-if="ready">
+    <NoUsersView v-if="users.length === 0" />
+  </template>
 </template>
---
+++ frontend/src/ui/views/NoUsersView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import EmptyUserList from "@/ui/components/users/EmptyUserList.vue";
+import CreateUserModal from "@/ui/components/users/CreateUserModal.vue";
+import {ref} from "vue";
+
+const showCreateUserModal = ref(false);
+</script>
+
+<template>
+  <EmptyUserList @create="showCreateUserModal = true" />
+  <CreateUserModal @dismiss="showCreateUserModal = false" v-if="showCreateUserModal" />
+</template>
